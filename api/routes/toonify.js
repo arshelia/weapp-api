960db2caefc92426f6b21ef346c4995f97e33863
shelia <shelia.wu4575@gmail.com>
2020-11-01 00:07:37 +0800

    fix(toonify): fix

--- api/routes/toonify.js
+++ api/routes/toonify.js
@@ -3,8 +3,8 @@
  * @version: 
  * @Author: shelia
  * @Date: 2020-10-30 10:36:04
- * @LastEditors: shelia
- * @LastEditTime: 2020-10-30 12:38:46
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2020-10-31 22:03:34
  */
 const express = require('express')
 const router = express.Router()
@@ -15,12 +15,12 @@ deepai.setApiKey('af2e7235-d674-4c78-92db-df64afb2df32');
 router.post('/', (req, res, next )=> {
     console.log(req.body)
     const image = req.body.image
-    console.log(image)
+    // console.log(image)
     async function start(image) {
         var resp = await deepai.callStandardApi("toonify", {
                 image: image,
         });
-        console.log(resp);
+        // console.log(resp);
         res.status(200).json({
 
             outputURL: resp.output_url
